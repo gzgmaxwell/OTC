1085e45746615c62180a01b8f92ff63ee6dd26fd
Andy <674244021@qq.com>
2026-03-02 21:01:39 +0800

    码商手机号和邮箱必填校验去掉

--- otc_menber_front/src/views/system/user/edit.vue
+++ otc_menber_front/src/views/system/user/edit.vue
@@ -12,64 +12,34 @@
       </div>
     </div>
     <div class="edit_content">
-      <el-form
-        class="u_form"
-        :model="formValidate"
-        :rules="rules"
-        ref="formValidate"
-        label-width="100px"
-      >
-        <el-form-item label="账号："   v-if="id.trim().length != 0" prop="userName">
+      <el-form class="u_form" :model="formValidate" :rules="rules" ref="formValidate" label-width="100px">
+        <el-form-item label="账号：" v-if="id.trim().length != 0" prop="userName">
           <!-- <el-input v-model="formValidate.userName"   v-if="id.trim().length != 0" ></el-input> -->
-          <span >{{  formValidate.userName }}</span>
+          <span>{{ formValidate.userName }}</span>
         </el-form-item>
-
-        <el-form-item label="姓名：" prop="fullName"  >
-          <el-input v-model="formValidate.fullName" :disabled="id.trim().length != 0"   ></el-input>
-        </el-form-item>        
-
-        
-
-        <el-form-item label="身份证号：" prop="idNumber" >
-          <el-input v-model="formValidate.idNumber"    :disabled="id.trim().length != 0"></el-input>
-        </el-form-item>        
-
-        <el-form-item label="手机号：" prop="phoneNum" >
-          <el-input v-model="formValidate.phoneNum"  :disabled="id.trim().length != 0" ></el-input>
+        <el-form-item label="姓名：" prop="fullName">
+          <el-input v-model="formValidate.fullName" :disabled="id.trim().length != 0"></el-input>
         </el-form-item>
-
-       <el-form-item label="邮箱地址：" prop="emailAddress">
-          <el-input v-model="formValidate.emailAddress"   ></el-input>
+        <el-form-item label="身份证号：" prop="idNumber">
+          <el-input v-model="formValidate.idNumber" :disabled="id.trim().length != 0"></el-input>
         </el-form-item>
-
-
+        <el-form-item label="手机号：">
+          <el-input v-model="formValidate.phoneNum" :disabled="id.trim().length != 0"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱地址：">
+          <el-input v-model="formValidate.emailAddress"></el-input>
+        </el-form-item>
         <el-form-item label="头像：" prop="header">
-
-          <el-upload
-                class="avatar-uploader"
-                :action="upload_url"
-                :show-file-list="false"
-                accept=".jpg, .jpeg, .JPG, .JPEG, .png"
-                :on-success="handleIconSuccess">
-                <img v-if="formValidate.header" :src="formValidate.header" class="avatar">
-                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-              </el-upload>
-
-
-        </el-form-item> 
-
-
-
-              
-
-
-
-
-
+          <el-upload class="avatar-uploader" :action="upload_url" :show-file-list="false"
+            accept=".jpg, .jpeg, .JPG, .JPEG, .png" :on-success="handleIconSuccess">
+            <img v-if="formValidate.header" :src="formValidate.header" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
         <el-form-item label="昵称：" prop="nickName">
-          <el-input v-model="formValidate.nickName"   ></el-input>
-        </el-form-item>        
-<!--         
+          <el-input v-model="formValidate.nickName"></el-input>
+        </el-form-item>
+        <!--         
         <el-form-item label="组织机构：" prop="inst">
 
           <el-select  v-model="formValidate.inst"  placeholder="请选择组织机构" style="width: 100%;">
@@ -78,7 +48,7 @@
 
 
         </el-form-item> -->
-<!-- 
+        <!-- 
         <el-form-item label="角色：" prop="post">
           <SelectTree
             :checkedKeys.sync="formValidate.post"
@@ -91,8 +61,8 @@
           />
         </el-form-item> -->
 
-         
-<!--          
+
+        <!--          
              <el-form-item label="所在县市" prop="szxs">
 
 
@@ -109,7 +79,7 @@
 
             </el-form-item> -->
 
-            
+
 
         <!-- <el-form-item v-if="id" label="密码：">
           <el-button type="primary" @click="comfirmpassword">
@@ -128,15 +98,15 @@
 </template>
 <script>
 import SelectTree from "@/components/tree/SelectTree.vue";
-import { AreaList,orgCascade } from "@a/system";
-import { UserInfo, UserSave, UserUpdate,  ResetPassword, PostCascader } from "@a/system";
+import { AreaList, orgCascade } from "@a/system";
+import { UserInfo, UserSave, UserUpdate, ResetPassword, PostCascader } from "@a/system";
 
 export default {
   name: "Edit",
   components: { SelectTree },
   data() {
     return {
-      inst:[],
+      inst: [],
       id: "",
       posts: [], //角色列表
       postProps: {
@@ -144,7 +114,7 @@ export default {
         label: "label",
         value: "value"
       },
-      orgs:[],
+      orgs: [],
       orgProps: {
         children: "children",
         label: "label",
@@ -185,16 +155,16 @@ export default {
     };
   },
   async mounted() {
-    if(this.$route.query.id){
+    if (this.$route.query.id) {
 
-    this.id = this.$route.query.id;
+      this.id = this.$route.query.id;
     }
     await this.getPosts();
     await this.getAreaCascade();
     if (this.id) {
       this.getInfo();
     } else {
-    
+
     }
 
     // this.getInst();
@@ -202,9 +172,9 @@ export default {
   methods: {
     //获取地区信息
     async getAreaCascade() {
-      const data  = await orgCascade();
-      this.orgs=data;
-     
+      const data = await orgCascade();
+      this.orgs = data;
+
     },
     change4(value) {
       if (value) {
@@ -214,11 +184,11 @@ export default {
         });
       }
     },
-      handleIconSuccess(res, file) {
-        this.formValidate.header = res.url;
-      },
-    async getInst(){
-      this.inst=await SysInstList();
+    handleIconSuccess(res, file) {
+      this.formValidate.header = res.url;
+    },
+    async getInst() {
+      this.inst = await SysInstList();
     },
     //获取角色Cascader
     async getPosts() {
@@ -301,27 +271,30 @@ export default {
 </script>
 
 <style>
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>

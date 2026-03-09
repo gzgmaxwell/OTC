e4215d40832637fcc7439c02face80cefd46f182
Andy <674244021@qq.com>
2026-03-09 18:39:11 +0800

    后台orderId 搜索条件添加

--- otc_front/src/views/transaction/payOrder/index.vue
+++ otc_front/src/views/transaction/payOrder/index.vue
@@ -2,12 +2,9 @@
   <div class="list_page">
     <div class="top_wrapper">
       <div class="search_box">
-        <el-input
-          placeholder="请输入付款人"
-          v-model="params.fromId"
-          @keyup.enter.native="search"
-        ></el-input>
-
+        <el-input placeholder="请输入付款人" v-model="params.fromId" @keyup.enter.native="search"></el-input>
+        <el-input placeholder="请输入订单编号" v-model="params.orderId" @keyup.enter.native="search"
+          style="margin-left: 10px;"></el-input>
         <el-button type="primary" icon="el-icon-search" @click="search">
           搜索
         </el-button>
@@ -17,29 +14,13 @@
 
     <div class="table_wrapper">
       <el-table ref="multipleTable" :data="list" border height="100%">
-        <el-table-column
-          prop="fromNickName"
-          label="付款人昵称"
-        ></el-table-column>
-
+        <el-table-column prop="fromNickName" label="付款人昵称"></el-table-column>
         <el-table-column prop="address" label="钱包地址"></el-table-column>
-
-        <el-table-column
-          prop="purposeNickName"
-          label="收款人昵称"
-        ></el-table-column>
-
+        <el-table-column prop="purposeNickName" label="收款人昵称"></el-table-column>
         <el-table-column prop="money" label="金额"></el-table-column>
-
         <el-table-column prop="title" label="标题"></el-table-column>
-
         <el-table-column prop="orderId" label="订单编号"></el-table-column>
-
-        <el-table-column
-          prop="orderStatusName"
-          label="订单状态"
-        ></el-table-column>
-
+        <el-table-column prop="orderStatusName" label="订单状态"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button size="mini" @click="edit(scope.row)">查看</el-button>
@@ -47,16 +28,9 @@
         </el-table-column>
       </el-table>
     </div>
-    <el-pagination
-      background
-      @size-change="sizeChange"
-      @current-change="changePage"
-      :current-page="params.current"
-      :page-sizes="[10, 20, 30]"
-      :page-size="params.size"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-    ></el-pagination>
+    <el-pagination background @size-change="sizeChange" @current-change="changePage" :current-page="params.current"
+      :page-sizes="[10, 20, 30]" :page-size="params.size" layout="total, sizes, prev, pager, next, jumper"
+      :total="total"></el-pagination>
   </div>
 </template>
 
@@ -82,7 +56,7 @@ export default {
       fileList: []
     };
   },
-  created() {},
+  created() { },
   methods: {
     //搜索
     search() {
@@ -113,7 +87,7 @@ export default {
           });
           this.delData(totalArr);
         })
-        .catch(() => {});
+        .catch(() => { });
     },
     //获取列表
     async List() {
@@ -178,7 +152,7 @@ export default {
           arr.push(row.id);
           this.delData(arr);
         })
-        .catch(() => {});
+        .catch(() => { });
     }
   },
   mounted() {

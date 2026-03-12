a09192ec27a64ca7fd6d9ceccf9d7537c5b733d8
Andy <674244021@qq.com>
2026-03-12 14:42:17 +0800

    enum.js

---
+++ otc_menber_front/src/utils/enum.js
@@ -0,0 +1,23 @@
+export const optConfigType = [
+  {
+    value: "1",
+    label: "系统配置"
+  },
+  {
+    value: "2",
+    label: "平台活动"
+  },
+  {
+    value: "3",
+    label: "开关"
+  }
+];
+export const timeFilterOptions = [
+  { value: "today", label: "当日" },
+  { value: "yesterday", label: "昨日" },
+  { value: "beforeThreeDays", label: "近三日" },
+  { value: "thisWeek", label: "本周" },
+  { value: "lastWeek", label: "上一周" },
+  { value: "lastMonth", label: "上一月" },
+  { value: "thisMonth", label: "本月" }
+]

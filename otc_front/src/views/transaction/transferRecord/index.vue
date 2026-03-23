575e82e1dbd5d8dba9f377fdf95135a1840f3f70
Andy <674244021@qq.com>
2026-03-23 16:50:10 +0800

    付款人编号ID

--- otc_front/src/views/transaction/transferRecord/index.vue
+++ otc_front/src/views/transaction/transferRecord/index.vue
@@ -4,6 +4,8 @@
       <div class="search_box">
         <el-input placeholder="付款人昵称" v-model="params.fromNickName" style="width: 30%; "
           @keyup.enter.native="search"></el-input>
+        <el-input placeholder="付款人编号ID" v-model="params.fromUserId" style="width: 30%;margin-left: 10px; "
+          @keyup.enter.native="search"></el-input>
         <el-input placeholder="订单编号" v-model="params.transNumber" style="width: 30%;margin-left: 10px; "
           @keyup.enter.native="search"></el-input>
 
@@ -35,16 +37,13 @@
       <el-table ref="multipleTable" :data="list" border height="100%" stripe style="width: 100%" :show-summary="true"
         :summary-method="getSummaries">
         <el-table-column prop="fromNickName" label="付款人昵称"></el-table-column>
-
+        <el-table-column prop="fromUserId" label="付款人编号ID"></el-table-column>
         <el-table-column prop="fromCodeName" label="付款人码商"></el-table-column>
-
         <!-- <el-table-column prop="address" label="钱包地址"></el-table-column> -->
         <el-table-column prop="transNumber" label="订单编号"></el-table-column>
         <el-table-column prop="zdlxName" label="类型"></el-table-column>
-
         <el-table-column prop="purposeNickName" label="收款人昵称"></el-table-column>
         <el-table-column prop="purposeCodeName" label="收款人码商"></el-table-column>
-
         <el-table-column prop="money" label="金额">
           <template slot-scope="scope">
             {{ formatCurrency(scope.row.money) }}

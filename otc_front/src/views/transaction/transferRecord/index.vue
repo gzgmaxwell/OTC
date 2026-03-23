7bd0e393beef99b2d29aa93f4bd969045176c7d8
Andy <674244021@qq.com>
2026-03-23 09:31:59 +0800

    付款人昵称

--- otc_front/src/views/transaction/transferRecord/index.vue
+++ otc_front/src/views/transaction/transferRecord/index.vue
@@ -2,53 +2,26 @@
   <div class="list_page">
     <div class="top_wrapper">
       <div class="search_box">
-        <el-input
-          placeholder="订单编号"
-          v-model="params.transNumber"
-          style="width: 30%; "
-          @keyup.enter.native="search"
-        ></el-input>
+        <el-input placeholder="付款人昵称" v-model="params.fromNickName" style="width: 30%; "
+          @keyup.enter.native="search"></el-input>
+        <el-input placeholder="订单编号" v-model="params.transNumber" style="width: 30%;margin-left: 10px; "
+          @keyup.enter.native="search"></el-input>
 
-        <el-select
-          filterable
-          v-model="params.fromInvitationCode"
-          placeholder="请选择付款人码商"
-          style="width: 30%; margin-left: 10px;"
-        >
-          <el-option
-            v-for="user in users"
-            :key="user.memberId"
-            :label="user.nickName + '-' + user.memberId"
-            :value="user.memberId"
-          ></el-option>
+        <el-select filterable v-model="params.fromInvitationCode" placeholder="请选择付款人码商"
+          style="width: 30%; margin-left: 10px;">
+          <el-option v-for="user in users" :key="user.memberId" :label="user.nickName + '-' + user.memberId"
+            :value="user.memberId"></el-option>
         </el-select>
 
-        <el-select
-          filterable
-          v-model="params.purposeInvitationCode"
-          placeholder="请选择收款人码商"
-          style="width: 30%; margin-left: 10px;"
-        >
-          <el-option
-            v-for="user in users"
-            :key="user.memberId"
-            :label="user.nickName + '-' + user.memberId"
-            :value="user.memberId"
-          ></el-option>
+        <el-select filterable v-model="params.purposeInvitationCode" placeholder="请选择收款人码商"
+          style="width: 30%; margin-left: 10px;">
+          <el-option v-for="user in users" :key="user.memberId" :label="user.nickName + '-' + user.memberId"
+            :value="user.memberId"></el-option>
         </el-select>
 
-        <el-date-picker
-          style="width: 50%; margin-left: 10px;"
-          @change="selectTime"
-          v-model="value2"
-          type="datetimerange"
-          :picker-options="pickerOptions"
-          value-format="yyyy-MM-dd HH:mm:ss"
-          range-separator="至"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-          align="right"
-        >
+        <el-date-picker style="width: 50%; margin-left: 10px;" @change="selectTime" v-model="value2"
+          type="datetimerange" :picker-options="pickerOptions" value-format="yyyy-MM-dd HH:mm:ss" range-separator="至"
+          start-placeholder="开始日期" end-placeholder="结束日期" align="right">
         </el-date-picker>
 
         <el-button type="primary" icon="el-icon-search" @click="search">
@@ -59,38 +32,18 @@
     </div>
 
     <div class="table_wrapper">
-      <el-table
-        ref="multipleTable"
-        :data="list"
-        border
-        height="100%"
-        stripe
-        style="width: 100%"
-        :show-summary="true"
-        :summary-method="getSummaries"
-      >
-        <el-table-column
-          prop="fromNickName"
-          label="付款人昵称"
-        ></el-table-column>
+      <el-table ref="multipleTable" :data="list" border height="100%" stripe style="width: 100%" :show-summary="true"
+        :summary-method="getSummaries">
+        <el-table-column prop="fromNickName" label="付款人昵称"></el-table-column>
 
-        <el-table-column
-          prop="fromCodeName"
-          label="付款人码商"
-        ></el-table-column>
+        <el-table-column prop="fromCodeName" label="付款人码商"></el-table-column>
 
         <!-- <el-table-column prop="address" label="钱包地址"></el-table-column> -->
         <el-table-column prop="transNumber" label="订单编号"></el-table-column>
         <el-table-column prop="zdlxName" label="类型"></el-table-column>
 
-        <el-table-column
-          prop="purposeNickName"
-          label="收款人昵称"
-        ></el-table-column>
-        <el-table-column
-          prop="purposeCodeName"
-          label="收款人码商"
-        ></el-table-column>
+        <el-table-column prop="purposeNickName" label="收款人昵称"></el-table-column>
+        <el-table-column prop="purposeCodeName" label="收款人码商"></el-table-column>
 
         <el-table-column prop="money" label="金额">
           <template slot-scope="scope">
@@ -98,32 +51,19 @@
           </template>
         </el-table-column>
 
-        <el-table-column
-          prop="updateTime"
-          label="更新时间"
-          width="160"
-        ></el-table-column>
+        <el-table-column prop="updateTime" label="更新时间" width="160"></el-table-column>
 
         <el-table-column label="操作" width="200">
           <template slot-scope="scope">
             <el-button size="mini" @click="edit(scope.row)">查看</el-button>
-            <el-button size="mini" type="danger" @click="Delete(scope.row)"
-              >删除</el-button
-            >
+            <el-button size="mini" type="danger" @click="Delete(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <el-pagination
-      background
-      @size-change="sizeChange"
-      @current-change="changePage"
-      :current-page="params.current"
-      :page-sizes="[10, 20, 30]"
-      :page-size="params.size"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-    ></el-pagination>
+    <el-pagination background @size-change="sizeChange" @current-change="changePage" :current-page="params.current"
+      :page-sizes="[10, 20, 30]" :page-size="params.size" layout="total, sizes, prev, pager, next, jumper"
+      :total="total"></el-pagination>
   </div>
 </template>
 
@@ -188,7 +128,7 @@ export default {
       fileList: []
     };
   },
-  created() {},
+  created() { },
   methods: {
     getSummaries({ columns, data }) {
       // 如果没有数据，返回空数组或默认值
@@ -274,7 +214,7 @@ export default {
           });
           this.delData(totalArr);
         })
-        .catch(() => {});
+        .catch(() => { });
     },
     //获取列表
     async List() {
@@ -344,7 +284,7 @@ export default {
           arr.push(row.id);
           this.delData(arr);
         })
-        .catch(() => {});
+        .catch(() => { });
     }
   },
   mounted() {

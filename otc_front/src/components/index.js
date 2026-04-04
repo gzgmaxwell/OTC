47fcf632e7e5c1a2e58df9a1c985184bc29210a7
Andy <674244021@qq.com>
2026-04-04 13:23:37 +0800

    console.log(1111,key, components[key]);

--- otc_front/src/components/index.js
+++ otc_front/src/components/index.js
@@ -13,7 +13,6 @@ const components = {
 Vue.use({
   install() {
     Object.keys(components).map(key => {
-      console.log(1111,key, components[key]);
       Vue.component(key, components[key]);
     });
   }

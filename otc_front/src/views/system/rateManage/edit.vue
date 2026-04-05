68227a5dce7b98b3e6117e3200d237f2d2a4117b
Andy <674244021@qq.com>
2026-04-05 22:02:53 +0800

    编辑

--- otc_front/src/views/system/rateManage/edit.vue
+++ otc_front/src/views/system/rateManage/edit.vue
@@ -2,7 +2,7 @@
   <div class="edit_page">
     <div class="top_box">
       <div class="title">
-        {{ this.id ? "编辑配置" : "新增配置" }}
+        {{ this.id ? "编辑费率" : "新增费率" }}
       </div>
       <div>
         <ja-button type="primary" :click="save">
@@ -154,32 +154,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-}
-
-.avatar-uploader .el-upload:hover {
-  border-color: #409eff;
-}
-
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-}
-
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
-</style>
